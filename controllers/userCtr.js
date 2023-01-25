c050aacc11a43a9af83b42c66eee76fdf45f61e4
Xhena Gura <xhena@teachpitch.com>
2023-01-25 15:34:57 +0100

    Error Handling & Login Functionality

--- controllers/userCtr.js
+++ controllers/userCtr.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const { generateToken } = require("../config/jwtToken");
 
-const createUser = async(req, res) => {
+const createUser = asyncHandler(async(req, res) => {
     const email = req.body.email;
 
     const findUser = await User.findOne({email: email});
@@ -12,13 +14,29 @@ const createUser = async(req, res) => {
         res.json(newUser);
     }else{
         // User already exists
-
-        res.json
-            ({
-                msg:"User Already Exists",
-                success:false
-            });
+        throw new Error("User Already Exists");
     }
-};
+});
 
-module.exports={createUser}
+
+const loginUserCtrl = asyncHandler(async(req, res) => {
+    const {email, password} = req.body;
+    console.log(email, password)
+
+    const findUser = await User.findOne({ email});
+    if(findUser && (await findUser.isPasswordMatched(password))) {
+
+        res.json({
+            _id:findUser?._id,
+            firstname:findUser?.firstname,
+            lastname:findUser?.lastname,
+            email:findUser?.email,
+            mobile:findUser?.mobile,
+            token:generateToken(findUser?._id)
+        });
+    }else{
+        throw new Error("Invalid Credentials")
+    }
+});
+
+module.exports={createUser, loginUserCtrl};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHanlder");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
@@ -10,7 +11,11 @@ dbConnect();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/api/user', authRouter)
+app.use('/api/user', authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 });
