c74c83daacb3b5c371024f93c6c4f64453e16078
Xhena Gura <xhena@teachpitch.com>
2023-01-25 17:07:53 +0100

    Get all users function

--- controllers/userCtr.js
+++ controllers/userCtr.js
@@ -39,4 +39,17 @@ const loginUserCtrl = asyncHandler(async(req, res) => {
     }
 });
 
-module.exports={createUser, loginUserCtrl};
+
+// Get all users
+
+const getallUsers = asyncHandler( async(req, res) => {
+    try{
+
+        const getUsers = await User.find();
+        res.json(getUsers)
+    }catch(error) {
+        throw new Error(error);
+    }
+});
+
+module.exports={createUser, loginUserCtrl, getallUsers};
