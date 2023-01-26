cd2036523cbe041c297befa3a2a2f254d2100a1e
Xhena Gura <xhena@teachpitch.com>
2023-01-26 17:15:50 +0100

    Protected Routes, How to Handle Refresh Tokens

--- controllers/userCtr.js
+++ controllers/userCtr.js
@@ -1,6 +1,9 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const { generateToken } = require("../config/jwtToken");
+const validateMongoDbId = require("../utils/validateMongodbid");
+const { generateRefreshToken } = require("../config/refreshToken");
+
 
 const createUser = asyncHandler(async(req, res) => {
     const email = req.body.email;
@@ -21,11 +24,20 @@ const createUser = asyncHandler(async(req, res) => {
 
 const loginUserCtrl = asyncHandler(async(req, res) => {
     const {email, password} = req.body;
-    console.log(email, password)
+    // console.log(email, password)
 
     const findUser = await User.findOne({ email});
     if(findUser && (await findUser.isPasswordMatched(password))) {
-
+        const refreshToken = await generateRefreshToken(findUser?.id);
+        const updateuser = await User.findByIdAndUpdate(findUser.id, {
+            refreshToken: refreshToken,
+        }, 
+        {new:true});
+        res.cookie('refreshToken', refreshToken,
+        {
+            httpOnly: true,
+            maxAge: 72 * 60 * 60 * 1000,
+        })
         res.json({
             _id:findUser?._id,
             firstname:findUser?.firstname,
@@ -40,6 +52,30 @@ const loginUserCtrl = asyncHandler(async(req, res) => {
 });
 
 
+// Update a user 
+
+const updatedUser = asyncHandler(async(req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try{
+
+        const updatedUser = await User.findByIdAndUpdate(
+            _id,
+            {
+                firstname: req?.body?.firstname,
+                lastname: req?.body?.lastname,
+                email: req?.body?.email,
+                mobile: req?.body?.mobile,
+            },
+            {
+                new:true
+            }
+        );
+        res.json(updatedUser);
+    }catch(error){
+        throw new Error(error);
+    }
+})
 // Get all users
 
 const getallUsers = asyncHandler( async(req, res) => {
@@ -52,4 +88,73 @@ const getallUsers = asyncHandler( async(req, res) => {
     }
 });
 
-module.exports={createUser, loginUserCtrl, getallUsers};
+
+// Get a single user 
+
+const getaUser = asyncHandler(async(req, res) => {
+    // console.log(req.params)
+    const {id} = req.params;
+    validateMongoDbId(id);
+    try{
+
+        const getaUser = await User.findById(id);
+        res.json({getaUser})
+    }catch(error){
+        throw new Error(error);
+    }
+})
+
+// Delete a user
+const deleteaUser = asyncHandler(async(req, res) => {
+    // console.log(req.params)
+    const {id} = req.params;
+    validateMongoDbId(id);
+    try{
+
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json({deleteaUser})
+    }catch(error){
+        throw new Error(error);
+    }
+})
+
+const blockUser = asyncHandler( async(req, res) => {
+    const {id} = req.params;
+    validateMongoDbId(id);
+
+    try{
+        const block = await User.findByIdAndUpdate(id, {
+            isBlocked:true,
+        },
+        {
+            new:true
+        }
+    );
+    res.json({
+        message:"User Blocked"
+    })
+    }catch(error){
+        throw new Error(error);
+    }
+})
+const unblockUser = asyncHandler( async(req, res) => {
+    const {id} = req.params;
+    validateMongoDbId(id);
+
+    try{
+        const unblock = await User.findByIdAndUpdate(id, {
+
+            isBlocked:false,
+        },
+        {
+            new:true
+        }
+    );
+    res.json({
+        message:"User UnBlocked"
+    })
+    }catch(error){
+        throw new Error(error);
+    }
+})
+module.exports={createUser, loginUserCtrl, getallUsers, getaUser, deleteaUser, updatedUser,blockUser, unblockUser};
--- models/userModel.js
+++ models/userModel.js
@@ -32,8 +32,25 @@ var userSchema = new mongoose.Schema({
     role:{
         type: String,
         default:"user",
+    },
+    isBlocked:{
+        type: Boolean,
+        default:false
+    },
+    cart:{
+        type:Array,
+        default: []
+    },
+    address: [{type: mongoose.Schema.Types.ObjectId, ref: "Address"}],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product"}],
+    refreshToken:{
+        type: String,
     }
-});
+},
+{
+    timestamps:true
+}
+);
 
 
 userSchema.pre('save', async function(next){
